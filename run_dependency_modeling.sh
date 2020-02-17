c10232008e41cb83fad78beceb6662f89baaead2
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-02-17 17:32:15 -0500

    Refactor runnables

--- run_dependency_modeling.sh
+++ run_dependency_modeling.sh
@@ -9,15 +9,7 @@
 #
 #SBATCH --ntasks=1
 
-export MODEL_NAME_OR_PATH=roberta-base
-#export MODEL_NAME_OR_PATH=/mnt/nfs/work1/miyyer/tuvu/torch/xlm-mlm-en-2048 #
-#export MODEL_NAME_OR_PATH=/mnt/nfs/work1/miyyer/tuvu/torch/spanbert_hf_base/
-#export MODEL_NAME_OR_PATH=xlnet-base-cased
-# bert-base-cased, bert-large-cased,
-# roberta-base, roberta-large,
-# xlnet-base-cased, xlnet-large-cased
-# xlm-mlm-en-2048
-#export MODEL_NAME_OR_PATH=/Users/tutvu/Downloads/spanbert_hf_base
+
 export DO_LOWER_CASE=False
 export CACHE_DIR=../cache
 export MAX_CONTEXT_SIZE=249
@@ -25,11 +17,15 @@ export MAX_SEQUENCE_LENGTH=512
 export SPAN_LENGTH=1
 export MAX_SPAN_LENGTH=1
 export MAX_NUM_EXAMPLES=3000
-export MODEL_NAME=roberta # bert, xlnet, xlm, roberta
-export DATA_NAME=RACE
 
+#model_name_or_path=('bert-base-cased', 'bert-large-cased', 'roberta-base', 'roberta-large', 'xlnet-base-cased', 'xlnet-large-cased', 'xlm-mlm-en-2048')
+#model_name=('bert', 'bert', 'roberta', 'roberta', 'xlnet', 'xlnet', 'xlm')
+#data_name=('RACE', 'aclImdb')
+
+export MODEL_NAME_OR_PATH=bert-base-cased
+export MODEL_NAME=bert # bert, xlnet, xlm, roberta
+export DATA_NAME=RACE # RACE, aclImdb
 export OUTPUT_DIR=../output_dir/${MODEL_NAME_OR_PATH}-${DATA_NAME}
-#../data/aclImdb/train \
 
 python dependency_modeling.py \
   --data_name ${DATA_NAME} \
