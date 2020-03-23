b7c6e8c9144612749f0c27a7057f37df1160b6db
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-03-23 03:31:55 -0400

    Generate tag data for roberta

--- run_all_tags.sh
+++ run_all_tags.sh
@@ -3,5 +3,5 @@
 #for tag in "FREQ" "INFREQ" "VERB" "PROPN" "SYM" "NOUN" "PART" "INTJ" "PUNCT" "ADP" "ADJ" "X" "ADV" "DET" "NUM" "PUNCT" "PRON"
 for tag in "PROPN" "NOUN" "ADJ" "ADV" "DET" "NUM"
 do
-    sbatch --gres=gpu:1 --partition=1080ti-long --mem=40GB --output=logsdepmodel_tags/dependency_modeling_${tag}_robert_aclImdb.txt dependency_modeling_tags.sh ${tag}
+    sbatch --gres=gpu:1 --partition=1080ti-long --mem=40GB --output=logsdepmodel_tags/dependency_modeling_${tag}_roberta_aclImdb.txt dependency_modeling_tags.sh ${tag}
 done
