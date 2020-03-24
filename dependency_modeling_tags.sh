abd1bcabd993851c52a8fba560e98ea76a2405c2
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-03-24 01:13:57 -0400

    Run tag data for XLM

--- dependency_modeling_tags.sh
+++ dependency_modeling_tags.sh
@@ -8,8 +8,8 @@ export MAX_SEQUENCE_LENGTH=512
 export SPAN_LENGTH=1
 export MAX_SPAN_LENGTH=1
 export MAX_NUM_EXAMPLES=3000 #3000
-export MODEL_NAME=xlnet # bert, xlnet, xlm, roberta
-export MODEL_NAME_OR_PATH=xlnet-base-cased
+export MODEL_NAME=xlm # bert, xlnet, xlm, roberta
+export MODEL_NAME_OR_PATH=/mnt/nfs/work1/696ds-s20/abajaj/nlplab/long-term-context/models/xlm-mlm-en-2048
 # bert-base-cased, bert-large-cased,
 # roberta-base, roberta-large,
 # xlnet-base-cased, xlnet-large-cased
@@ -18,7 +18,13 @@ export MODEL_NAME_OR_PATH=xlnet-base-cased
 export DATA_NAME=aclImdb
 export TAG=$1 # NOUN,PROPN,PRON,VERB,ADJ,ADP,DET,FREQ,INFREQ (get from run_all_tags.sh)
 
-export OUTPUT_DIR=../output_dir/${MODEL_NAME_OR_PATH}-${DATA_NAME}-${TAG}
+export CHECK_XLM="xlm"
+if [ "${MODEL_NAME}" = "${CHECK_XLM}" ]
+then export OUTPUT_DIR=../output_dir/xlm-mlm-en-2048-${DATA_NAME}-${TAG}
+else
+  export OUTPUT_DIR=../output_dir/${MODEL_NAME_OR_PATH}-${DATA_NAME}-${TAG}
+fi
+
 #../data/aclImdb/train \
 
 python dependency_modeling_tags.py \
--- run_all_tags.sh
+++ run_all_tags.sh
@@ -3,5 +3,5 @@
 #for tag in "FREQ" "INFREQ" "VERB" "PROPN" "SYM" "NOUN" "PART" "INTJ" "PUNCT" "ADP" "ADJ" "X" "ADV" "DET" "NUM" "PUNCT" "PRON"
 for tag in "PROPN" "NOUN" "ADJ" "ADV" "DET" "NUM"
 do
-    sbatch --gres=gpu:1 --partition=1080ti-long --mem=40GB --output=logsdepmodel_tags/dependency_modeling_${tag}_xlnet_aclImdb.txt dependency_modeling_tags.sh ${tag}
+    sbatch --gres=gpu:1 --partition=1080ti-long --mem=40GB --output=logsdepmodel_tags/dependency_modeling_${tag}_xlm_aclImdb.txt dependency_modeling_tags.sh ${tag}
 done
