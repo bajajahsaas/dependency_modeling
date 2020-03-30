3bd94fe3420f5446a673f1baf4b98f1e2d648aec
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-03-30 02:15:26 -0400

    Generate tags data for RACE and xlm

--- generate_tags_data.sh
+++ generate_tags_data.sh
@@ -13,8 +13,8 @@
 # roberta-base, roberta-large,
 # xlnet-base-cased, xlnet-large-cased
 # /mnt/nfs/work1/696ds-s20/abajaj/nlplab/long-term-context/models/xlm-mlm-en-2048
-export MODEL_NAME_OR_PATH=xlnet-base-cased
-export MODEL_NAME=xlnet # bert, xlnet, xlm, roberta
+export MODEL_NAME_OR_PATH=/mnt/nfs/work1/696ds-s20/abajaj/nlplab/long-term-context/models/xlm-mlm-en-2048
+export MODEL_NAME=xlm # bert, xlnet, xlm, roberta
 export DATA_NAME=RACE # RACE, aclImdb
 export DO_LOWER_CASE=False
 export CACHE_DIR=../cache
